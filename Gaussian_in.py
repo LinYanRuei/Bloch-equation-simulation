f2438b285459522dc0e548638a708815f8997d05
LinYanRuei <104285641+LinYanRuei@users.noreply.github.com>
2022-08-01 21:54:30 +0800

    Add files via upload

---
+++ Gaussian_in.py
@@ -0,0 +1,178 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import my_function as fun
+import hdf5Reader
+from scipy.optimize import curve_fit as fit
+from sys import float_info
+
+
+#Define parameter
+argDicts = {
+    'att': 1.43353e2,   #dB
+    'gain': 1.03127e2,  #dB
+    'ke': 4.215e15,     
+    'decoh': 1.143e6,   #Hz
+    'relax': 2.232e6,   #Hz
+    #Time
+    'tau': 1,
+    't0': 2.718,
+    'span': 7.5,
+    'dt': 0.001,
+}
+
+expDicts = {
+    'pwr_range': np.linspace(-20, 20, 21),
+    'detune_range': np.linspace(-3.6,3.6,37)
+}
+
+dirDicts = {
+    #pwr_dep
+    # 'datadir': r'C:\Users\user\OneDrive - 國立中正大學\桌面\job5\Data_sigma1us_Both4.766GHz_sweep_power\2022\07\Data_0720',
+    # 'prefix': '\Chalmers_TD_timedelay_1us_pwr_',
+    # 'suffix': 'dBm',
+    # 'ext': '.hdf5',
+    
+    'datadir': r'C:\Users\user\OneDrive - 國立中正大學\桌面\job111\Data_sigma1us_Both4.766GHz_sweep_power2\2022\07\Data_0720',
+    'prefix': '\Chalmers_TD_timedelay_1us_pwr_',
+    'suffix': 'dBm',
+    'ext': '.hdf5',
+    
+    #detune
+    # 'datadir': r'C:\Users\user\OneDrive - 國立中正大學\桌面\job5\Data_sigma1us_probe4.766GHz_sweep_flux\2022\07\Data_0720',
+    # 'prefix': '\Chalmers_TD_timedelay_1us_freq_tune_',
+    # 'suffix': 'MHz',
+    # 'ext': '.hdf5',
+
+}
+
+detune = 0          #Input_power
+power = -20   
+state = fun.g_state
+state_save = []
+q = fun.calculation_rk4(argDicts,power,detune,expDicts,dirDicts)
+t_range = q.t_range
+unit = q.r*2*np.pi*10/q.ke
+sigX, sigY, sigZ = q.pauli_matrix(t_range,state,state_save)
+
+input_chip = q.W_in(t_range)
+input_V = input_chip*unit
+input_V_digitizer = fun.dBm2vp(fun.vp2dBm(input_V)+q.att)    #Normal temperture
+output_chip = q.W_out(sigX,sigY,t_range)
+output_V = output_chip*unit                                  #Volt
+output_digitizer = fun.dBm2vp(fun.vp2dBm(output_V)+q.gain)   #Normal temperture
+output_BG = fun.dBm2vp(fun.vp2dBm(input_V)+q.gain)           #Normal temperture
+t_range_use = t_range*1e-6/(q.r*2*np.pi*1e-6)
+
+z_BG, x_BG, xname_BG = q.getData(True)     #BG
+zz,xx,xxname = q.getData(False)      #EXP_OUT
+
+plt.plot(t_range_use,output_digitizer)
+plt.plot(xx,np.abs(zz[:,0]))
+plt.show()
+for i in range(len(q.pwr_range)):
+    pwr = q.pwr_range[i]
+    q = fun.calculation_rk4(argDicts,pwr,detune,expDicts,dirDicts)
+    state_save=[]
+    sigX,sigY,sigZ= q.pauli_matrix(t_range,state,state_save)             
+    output_digitizer = fun.dBm2vp(fun.vp2dBm(q.W_out(sigX,sigY,t_range)*unit )+q.gain)
+    plt.plot(t_range_use,output_digitizer,'--')
+    plt.scatter(xx,np.abs(zz[:,i]),s=0.1,c ='red',marker= '.')
+    plt.title(f'{pwr:.0f}dbm')
+    plt.show()
+    # plt.savefig(r'C:/Users/user/OneDrive - 國立中正大學/桌面/Plot_result/{}dBm.png'.format(pwr),dpi=300)
+    # plt.clf()
+
+
+data_range = np.arange(0,21,1)
+t_plot = np.linspace(0,6,6981)*1e-6
+############################################# Pwr_dep
+# result_raw_BG = []
+
+# q.plot_result(data_range,t_plot,z_BG,221)
+# q.plot_result(data_range,t_plot,zz,223)
+
+# pwr_range = np.linspace(-20,20,21)
+# result = []
+# for pwr in pwr_range:
+#     q = fun.calculation_rk4(argDicts,pwr,detune,expDicts,dirDicts)
+#     result.append(q.W_in(t_range)*unit)
+# result = np.array(result)
+# q.plot_result_2(t_range_use,pwr_range,result,222)
+
+# result = []
+# for pwr in pwr_range:
+#     q = fun.calculation_rk4(argDicts,pwr,detune,expDicts,dirDicts)
+#     state_save = []           
+#     sigX,sigY = q.pauli_matrix(t_range,state,state_save)             
+#     output_digitizer = fun.dBm2vp(fun.vp2dBm(q.W_out(sigX,sigY,t_range)*unit )+q.gain) 
+#     result.append(output_digitizer)
+# q.plot_result_2(t_range_use,pwr_range,result,224)
+# plt.show()
+"""
+detuning
+q.plot_result(q.detune_range,t_plot,zz,223)
+
+detune_range = np.linspace(-3.6,3.6,37)
+result = []
+for det in detune_range:
+    q = fun.calculation_rk4(argDicts,power,det,expDicts,dirDicts)
+    result.append(q.W_in(t_range)*unit)
+result = np.array(result)
+q.plot_result_2(t_range_use,detune_range,result,222)
+q.plot_result_2(t_range_use,detune_range,result,221)
+
+result = []
+for det in detune_range:
+    print(det)
+    q = fun.calculation_rk4(argDicts,power,det,expDicts,dirDicts)          
+    sigX,sigY = q.pauli_matrix(t_range,state,[])          
+    output_digitizer = fun.dBm2vp(fun.vp2dBm(q.W_out(sigX,sigY,t_range)*unit)+q.gain) 
+    result.append(output_digitizer)
+q.plot_result_2(t_range_use,detune_range,result,224)
+# plt.show()
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# popt,pcov = fit(q.fit_func,x_BG,np.abs(z_BG[:,2]))
+# plt.plot(x_BG,q.fit_func(x_BG,*popt))
+# plt.show()
+# result_fit_in = []
+# for i in data_range:
+#     popt,pcov = fit(q.fit_func,x_BG,np.abs(z_BG[:,i]))
+#     result_fit_in.append(q.fit_func(x_BG,*popt))
+# result_fit_in = np.array(result_fit_in)
+# X,Y = np.meshgrid(t_plot,data_range)
+# cp_fit_in = plt.contourf(X,Y,result_fit_in,1000,cmap='jet')
+# cbar_fit_in = plt.colorbar(cp_fit_in)
+# cbar_fit_in.ax.set_ylabel('amplitude')
+# plt.title('Fit data (In)')
+# plt.ylabel('Input (power)')
+# plt.xlabel('time')
+# plt.show()
---
+++ input_output_2.py
@@ -0,0 +1,129 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from qutip import *
+import math
+import sympy as sy
+import scipy.integrate as integrate
+import scipy.interpolate as interpld
+import os
+# import fittingModels as fm
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+# Bloch equation
+def bloch(p,steps,args,omega_in,delta,v,tau):   
+    x,y,z = p
+    I = W_in(v,tau,omega_in)   #Omega_in
+    Q,r,gamma = args
+    dx = -r*x - delta*y + I*z
+    dy = delta*x - r*y - Q*z
+    dz = -I*x + Q*y - gamma*z - gamma
+    return [x+dx*steps,y+dy*steps, z+dz*steps]
+
+# Calculation
+def calculation(p,steps,args,omega_in,delta,tau):
+    c = []
+    for v in t:
+        p = bloch(p,steps,args,omega_in,delta,v,tau)
+        c.append(p)
+    return c
+
+def W_out(x,y,tau,omega_in,t):
+    return np.abs(W_in(t,tau,omega_in)+gamma*(x-y*1j))
+
+
+def sigma(p,steps,args,omega_in,delta,tau):
+    return np.array(calculation(p,steps,args,omega_in,delta,tau))
+
+def W_in(t,tau,omega_in):
+    # return omega_in*np.exp(t/tau)*(np.heaviside(-t, 0))  exponential
+    return omega_in*np.exp(-((t-15)/np.sqrt(2)*tau)**2)
+
+def efficiency(tau,omega_in):
+    cw = []
+    x = []
+    y = []
+    cw = calculation(p,steps,args,omega_in,delta,tau)
+    cw = np.array(cw)
+    x = cw[:, 0]
+    y = cw[:, 1]
+    t0 = np.argmax(W_out(x,y,tau,omega_in,t))
+    q = W_out(x,y,tau,omega_in,t)
+    # t0 = np.argmax(W_out(x,y,tau,omega_in,t))
+    # q = W_out(x,y,tau,omega_in,t)
+    rabiout_s = integrate.simps((q[t0:]**2), t[t0:])
+    rabiin_s = integrate.simps((W_in(t,tau,omega_in))**2,t)
+    rate_simps=rabiout_s/rabiin_s
+    return rate_simps
+# Parameter
+
+omega_in = 0.1
+gamma = 2
+delta = 0
+r = 1
+args = [0,r,gamma]
+steps = 0.001
+t = np.arange(0,30,steps)
+step = 0.01
+# Initial state
+p0 = [0.,0.,-1.]
+p = p0
+d = []
+c = []
+tau = 1
+
+k = np.array(sigma(p,steps,args,omega_in,delta,tau))
+sigX = k[:, 0]
+sigY = k[:, 1]
+sigZ = k[:, 2]
+
+
+# t0 = np.argmax(W_out(sigX,sigY,tau,omega_in,t))
+# c = []
+# c = W_out(sigX,sigY,tau,omega_in,t)
+t0 = np.argmax(W_out(sigX,sigY,tau,omega_in,t))
+c = []
+c = W_out(sigX,sigY,tau,omega_in,t)
+
+p1 = plt.plot(t,W_in(t,tau,omega_in),label = 'Input_pulse')
+p2 = plt.plot(t,W_out(sigX,sigY,tau,omega_in,t),label = 'Output_pulse')
+plt.title('Input & output pulse')
+plt.legend(loc = 'upper right')
+plt.xlabel('time')
+plt.ylabel('Amplitude')
+plt.show()
+print(efficiency(tau,omega_in))
+
+result = []
+rf_range = np.arange(0.01,3.01,step)
+tau_range = np.arange(0.2,0.5,step/10)
+print(len(rf_range),len(tau_range),len(k))
+print(sigX)
+
+result_line = []
+for j in range(len(tau_range)):
+    rf = 0.1
+    tau = tau_range[j]
+    result_line.append(efficiency(tau,rf))
+
+plt.plot(tau_range,result_line)
+plt.show()
+for i in range(len(rf_range)):
+    result.append([])
+    rf = rf_range[i]
+    print(i)
+    for j in range(len(tau_range)):
+        tau = tau_range[j]
+        result[i].append(efficiency(tau,rf))
+
+
+
+result = np.array(result)
+X,Y = np.meshgrid(tau_range, rf_range)
+cp = plt.contourf(X,Y,result,100,cmap='jet')
+
+cbar = plt.colorbar(cp)
+cbar.ax.set_ylabel('efficiency')
+plt.title('Efficiency in different tau and rabi frequency')
+plt.ylabel('Rabi frequency')
+plt.xlabel('tau')
+plt.show()
